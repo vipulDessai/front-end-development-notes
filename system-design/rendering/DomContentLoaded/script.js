68ea13d8e59157f2d9f80af82a32b45a28349f5a
Vipul Dessai <badgamerbad@gmail.com>
2025-03-30 23:47:37 +0530

    notes

---
+++ system-design/rendering/DomContentLoaded/script.js
@@ -0,0 +1,25 @@
+const log = document.querySelector(".event-log-contents");
+const reload = document.querySelector("#reload");
+
+reload.addEventListener("click", () => {
+  log.textContent = "";
+  setTimeout(() => {
+    window.location.reload(true);
+  }, 200);
+});
+
+window.addEventListener("load", (event) => {
+  log.textContent += "load\n";
+});
+
+document.addEventListener("readystatechange", (event) => {
+  log.textContent += `readystate: ${document.readyState}\n`;
+});
+
+document.addEventListener("DOMContentLoaded", (event) => {
+  log.textContent += "DOMContentLoaded\n";
+});
+
+function bodyOnLoad(event) {
+  log.textContent += "body onload\n";
+}
---
+++ system-design/testing/testing-type.md
@@ -0,0 +1,7 @@
+# Regresion Testing
+
+- if new changes are added then other changes should NOT break
+
+# Performance Testing
+
+- Speed Testing
